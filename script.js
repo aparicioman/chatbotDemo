a442156c590c2d9e71b8fa076e4fd39588802036
Javi <javier.aparicio.le@gmail.com>
2024-05-24 12:45:53 +0200

    New changes added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     // Mensaje informativo inicial
-    addMessageToChat('Chatbot', '¡Hola! Soy el asistente virtual de Javier Aparicio, esta es una demo para mis clientes de Workana. ¿En qué puedo ayudarte hoy?<br><br>Puedes preguntar acerca de los siguientes puntos:<br><br>1. Servicios que realizo<br>2. Preguntar por precios<br>3. Preguntar por los proyectos realizados');
+    addMessageToChat('Chatbot', '¡Hola! Soy el asistente virtual de Javier Aparicio, esta es una demo para mis clientes de Workana. ¿En qué puedo ayudarte hoy?<br><br>Puedes preguntar acerca de los siguientes puntos:<br><br>1. Servicios que realizo<br>2. Preguntar por precios<br>3. Preguntar por los proyectos realizados<br>4. Pídeme reproducir una canción<br>5. Pídeme detener la canción');
 });
 
 document.getElementById('chat-form').addEventListener('submit', function(event) {
@@ -31,11 +31,19 @@ document.getElementById('chat-form').addEventListener('submit', function(event) 
                 botResponse = 'Los precios dependerán del proyecto, por lo general el precio suele ser muy asumible y se ajusta a las necesidades de cada uno :)';
                 break;
             case 'servicios':
-                botResponse = 'Los ámbitos en los que estoy especializado desarrollo de software con c, desarrollo de bots y desarrollo de aplicaciones en Android con java';
+                botResponse = 'Los ámbitos en los que estoy especializado son:<br>- Desarrollo de software con C<br>- Desarrollo de bots<br>- Desarrollo de aplicaciones en Android con Java<br>';
                 break;
             case 'proyectos':
-                    botResponse = 'He realizado múltiples proyectos relacionados con el desarrollo de soluciones software a lo largo de mi vida entre ellos destacan el desarrollo de un bot especializado en cualquier ámbito de negocio o el desarrollo de una aplicación móvil para monitorear las comunicaciones BLE entre 2 dispositivos';
-                    break;
+                botResponse = 'He realizado múltiples proyectos relacionados con el desarrollo de soluciones software a lo largo de mi vida, entre ellos destacan:<br><br>- Desarrollo de una aplicación móvil para monitorear las comunicaciones BLE entre 2 dispositivos:<br><br><img src="images/bleConnectionCapture.png" alt="Proyectos" style="max-width: 100%; height: auto; border: 2px solid #000;"><br><br>- Desarrollo de un bot especializado en cualquier ámbito de negocio:<br><br><img src="images/TecnocasaBotCapture.png" alt="Proyectos" style="max-width: 100%; height: auto; border: 2px solid #000;">';
+                break;
+            case 'cancion':
+                botResponse = '¡Claro!, reproduciré una canción para tí';
+                playAudio();
+                break;
+            case 'parar_cancion':
+                botResponse = 'Ahora mismo la paro';
+                stopAudio();
+                break;
             // Añade más casos según tus intents
             default:
                 botResponse = 'No estoy seguro de cómo responder a eso.';
@@ -63,3 +71,13 @@ function addMessageToChat(sender, message) {
     chatBox.scrollTop = chatBox.scrollHeight; // Desplazar hacia abajo
 }
 
+function playAudio() {
+    let audioElement = document.getElementById('my-audio');
+    audioElement.play();
+}
+
+function stopAudio() {
+    let audioElement = document.getElementById('my-audio');
+    audioElement.pause();
+    audioElement.currentTime = 0;
+}
